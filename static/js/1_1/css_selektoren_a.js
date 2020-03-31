a2c923b21443b702bf2502da0717620bb2752bb3
derechtenap <50623315+derechtenap@users.noreply.github.com>
2020-03-31 13:07:43 +0200

    Aufgabe 1.1

---
+++ static/js/1_1/css_selektoren_a.js
@@ -0,0 +1,4 @@
+// Daten-Attribut nur verwendet, um beide Lösungen auf 
+// einer Seite zu haben. Für die Aufgabe ist eigentlich 
+// >> $('tr') << richtig 
+$('#tabelleA tr').css('background-color', 'lightblue')
---
+++ static/js/1_1/css_selektoren_b.js
@@ -0,0 +1,4 @@
+// Daten-Attribut nur verwendet, um beide Lösungen auf 
+// einer Seite zu haben. Für die Aufgabe ist eigentlich 
+// >> $('tr') << richtig
+$('#tabelleB tr:even').css('background-color', 'lightblue')
